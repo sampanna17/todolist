d49d00787bdd1f65dbb50714a85f924d2850ecbb
sampanna17 <sampannapiya13@gmail.com>
2024-07-28 18:03:26 +0545

    Added array for the listing and used usestyate for rendering

--- ToDoList/src/App.jsx
+++ ToDoList/src/App.jsx
@@ -1,29 +1,52 @@
-import React, { useState } from "react";
-import { MdDelete, MdMargin } from "react-icons/md";
+import React, { useEffect, useState } from "react";
+import { MdDelete } from "react-icons/md";
 import { FaCheck } from "react-icons/fa";
 import { FaEdit } from "react-icons/fa";
 import "./App.css";
 
 function App() {
     const [isCompleteScreen, setIsCompleteScreen] = useState(false);
+    const [allToDos, setToDos] = useState([]);
+    const [newTitle, setNewTitle] = useState("");
+    const [newDescription, setNewDescription] = useState("");
+
+    const handleAddToDo = ()=>{
+        let newToDoItem = {
+            title: newTitle,
+            description: newDescription,
+        };
+
+        let updatedToDoArray = [...allToDos];
+        updatedToDoArray.push(newToDoItem);
+        setToDos(updatedToDoArray);
+        localStorage.setItem('todolist',JSON.stringify(updatedToDoArray));
+
+    };
+
+    useEffect(()=>{
+        let savedToDo = JSON.parse(localStorage.getItem('todolist'))
+        if(savedToDo){
+            setToDos(savedToDo);
+        }
+    },[])
+
     return (
         <div className="App">
             <div className="todo-head">
                 <h1>My ToDos</h1>
             </div>
-
             <div className="todo-wrapper">
                 <div className="todo-input">
                     <div className="todo-input-item">
                         <label>Title</label>
-                        <input type="text" placeholder="Task Title" />
+                        <input type="text" value={newTitle} onChange={(e) => setNewTitle(e.target.value)} placeholder="Task Title" />
                     </div>
                     <div className="todo-input-item">
                         <label>Description</label>
-                        <input type="text" placeholder="Description" />
+                        <input type="text" value={newDescription} onChange={(e) => setNewDescription(e.target.value)} placeholder="Description" />
                     </div>
                     <div className="todo-input-item">
-                        <button type="buttton" className="primaryBtn">
+                        <button type="buttton" onClick={handleAddToDo} className="primaryBtn">
                             Add
                         </button>
                     </div>
@@ -45,22 +68,25 @@ function App() {
                 </div>
 
                 <div className="todo-list">
-                    <div className="todo-list-item">
-                        <div className="list-item">
-                            <h3>Task-1</h3>
-                            <p>Description-1</p>
-                        </div>
+                    {allToDos.map((item, index) => {
+                        return (
+                            <div className="todo-list-item" key={index}>
+                                <div className="list-item">
+                                    <h3>{item.title}</h3>
+                                    <p>{item.description}</p>
+                                </div>
 
-                        <div>
-                            <MdDelete className="icon" />
-                            <FaCheck className="check-icon" />
-                            <FaEdit className="edit-icon"/>
-                        </div>
-                    </div>
+                                <div>
+                                    <MdDelete className="icon" />
+                                    <FaCheck className="check-icon" />
+                                    <FaEdit className="edit-icon" />
+                                </div>
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
         </div>
     );
 }
-
 export default App;
